c2e9fb089e637536814956de1b7ff38717dbf5b4
mayur <mayurkukde84@gmail.com>
2023-02-14 00:30:26 +0530

    complete navbar

--- navbar/src/components/navbar/Navbar.jsx
+++ navbar/src/components/navbar/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import "./navbar.css";
+import {AiOutlineMenu} from 'react-icons/ai'
+import {HiMenuAlt3} from 'react-icons/hi'
 const Navbar = () => {
+    const [toggleMenu,setToggleMenu] = useState(true)
+    
   return (
     <div className="container">
       <div className="nav-container">
@@ -14,7 +18,27 @@ const Navbar = () => {
           <a href="about">About Us</a>
           <a href="contact">Contact Us</a>
         </div>
+        <div className="burger-container">
+        {
+            toggleMenu ? <HiMenuAlt3 size={20} onClick={()=>{setToggleMenu(false)}} /> : 
+            <AiOutlineMenu size={20}  onClick={()=>{setToggleMenu(true)}} />
+        }
+        
+        </div>
       </div>
+      {
+        toggleMenu && (
+            <div className="nav-links-burger">
+          <ul>
+            <li><a href="home">Home</a></li>
+            <li> <a href="services">Services</a></li>
+            <li><a href="career">Career</a></li>
+            <li> <a href="about">About Us</a></li>
+            <li>  <a href="contact">Contact Us</a></li>
+          </ul>
+        </div> 
+        )
+      }
     </div>
   );
 };
